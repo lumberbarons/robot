dd9ac06c786b0cd7d9f7e5b4500642d1f8ffbca4
James Ellwood <blindstareonline@hotmail.com>
2013-11-09 17:56:17 -0500

    Stepper controller fixed so 200 steps/rev is now correct.

--- websockets/stepper.py
+++ websockets/stepper.py
@@ -6,14 +6,18 @@ class StepperWriter:
     addr = 0x20
     bus = smbus.SMBus(1)
     
-    rotation = 800
+    rotation = 200
 
     def forward(self):
         for i in range(0, self.rotation):
             self.bus.write_byte_data(self.addr, 0x00, 0x00)
-            time.sleep(0.001)
+            time.sleep(0.002)
+        time.sleep(0.005)
+        self.bus.write_byte_data(self.addr, 0x02, 0x00)
             
     def backward(self):
         for i in range(0, self.rotation):
             self.bus.write_byte_data(self.addr, 0x01, 0x00)
-            time.sleep(0.001)
+            time.sleep(0.002)
+        time.sleep(0.005)
+        self.bus.write_byte_data(self.addr, 0x02, 0x00)
